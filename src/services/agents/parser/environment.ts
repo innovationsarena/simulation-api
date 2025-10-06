7e0f8731fb4be461e8d07f7f2f27e97172d55e05
gr-johan-kivi <johan.kivi@goteborgsregionen.se>
2025-10-06 09:55:48 +0200

    fix: enviroment fix

--- src/services/agents/parser/environment.ts
+++ src/services/agents/parser/environment.ts
@@ -5,7 +5,27 @@ export const parseEnviroment = async (
 ): Promise<string> => {
   const environment = await getEnvironment(environmentId);
 
-  let result = `## Environment\n\n ${environment}`;
+  let result = `## Environment\n The simulation takes place in the following context:\n\n`;
+
+  if (environment.description) {
+    result += `${environment.description}\n\n`;
+  }
+
+  if (environment.objectives) {
+    result += `${environment.objectives.map(
+      (o) => `### Objectives\n- ${o}\n`
+    )}\n`;
+  }
+
+  if (environment.constraints) {
+    result += `${environment.constraints.map(
+      (o) => `### Constraints\n- ${o}\n`
+    )}\n`;
+  }
+
+  if (environment.values) {
+    result += `${environment.values.map((o) => `### Values\n- ${o}\n`)}\n`;
+  }
 
   return result + "\n\n";
 };
