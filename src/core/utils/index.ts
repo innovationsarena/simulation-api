f2292ee6f03adeb00825dbd28140e08cf13abf48
gr-johan-kivi <johan.kivi@goteborgsregionen.se>
2025-10-16 13:10:14 +0200

    cleanup

--- src/core/utils/index.ts
+++ src/core/utils/index.ts
@@ -1,23 +1,3 @@
-import type { Message } from "../types";
-
-export const toLocalISO = (date: Date) => {
-  const offsetMs = date.getTimezoneOffset() * 60 * 1000;
-  const msLocal = date.getTime() - offsetMs;
-  const dateLocal = new Date(msLocal);
-  const iso = dateLocal.toISOString();
-  const isoLocal = iso.slice(0, 19);
-  return isoLocal;
-};
-
-export const parseMessages = (messages: Message[], senderid: string): any[] => {
-  return messages.map((message) => {
-    return {
-      role: message.senderId === senderid ? "user" : "assistant",
-      content: message.content,
-    };
-  });
-};
-
 export * from "./constants";
 export * from "./errorHandler";
 export * from "./generators";
